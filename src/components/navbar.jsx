a2b306941e24b2e4a039f4189c0fa6a1230c1434
ElBuenParker <mr.gonzalex.4@gmail.com>
2024-04-11 23:11:24 -0600

    header added

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -9,15 +9,15 @@ function Navbar() {
     //cuando esta true lo pasa a false y vice versa
     setClicked(!clicked)
   }
-  return (
+    return (
     <>
       <NavContainer>
         <h2>ElBuen<span>Parker</span></h2>
         <div className={`links ${clicked ? 'active' : ''}`}>
-          <a onClick={handleClick} href="#h">Inicio</a>
-          <a onClick={handleClick} href="#h">Portafolio</a>
-          <a onClick={handleClick} href="#h">Acerca de mi</a>
-          <a onClick={handleClick} href="#h">Contacto</a>
+          <button onClick={handleClick} href="#h" className="BntNav">Inicio</button>
+          <button onClick={handleClick} href="#h" className="BntNav">portafolio</button>
+          <button onClick={handleClick} href="#h" className="BntNav">Acerca de mi</button>
+          <button onClick={handleClick} href="#h" className="BntNav">Contacto</button>
         </div>
         <div className='burguer'>
           <BurguerButton clicked={clicked} handleClick={handleClick} />
@@ -32,22 +32,21 @@ export default Navbar
 
 const NavContainer = styled.nav`
   h2{
-    color: white;
+    color: black;
     font-weight: 400;
     span{
       font-weight: bold;
     }
   }
   padding: .4rem;
-  background-color: #333;
+  border: 2px solid #264143;
+  border-radius: 5px;
+  box-shadow: 3px 4px 0px 1px #000000;
+  background-color: #D1D1D1;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  a{
-    color: white;
-    text-decoration: none;
-    margin-right: 1rem;
-  }
+
   .links{
     position: absolute;
     top: -700px;
@@ -57,19 +56,11 @@ const NavContainer = styled.nav`
     margin-right: auto;
     text-align: center;
     transition: all .5s ease;
-    a{
-      color: white;
-      font-size: 2rem;
-      display: block;
-    }
+ 
     @media(min-width: 768px){
       position: initial;
       margin: 0;
-      a{
-        font-size: 1rem;
-        color: white;
-        display: inline;
-      }
+  
       display: block;
     }
   }
@@ -83,21 +74,42 @@ const NavContainer = styled.nav`
     left: 0;
     right: 0;
     text-align: center;
-    a{
-      font-size: 2rem;
-      margin-top: 1rem;
-      color: white;
-    }
+  
   }
   .burguer{
     @media(min-width: 768px){
       display: none;
     }
   }
+  .BntNav {
+    background-color: white;
+    color: black;
+    margin: 5px;
+    border-radius: .4em;
+    font-size: 17px;
+    font-weight: 600;
+    padding: 1em 2em;
+    cursor: pointer;
+    transition: all 0.3s ease-in-out;
+    border: 1px solid black;
+    box-shadow: 0 0 0 0 black;
+  }
+  
+  .BntNav:hover {
+    transform: translateY(-4px) translateX(-2px);
+    box-shadow: 2px 5px 0 0 black;
+  };
+  
+  .BntNav:active {
+    transform: translateY(2px) translateX(1px);
+    box-shadow: 0 0 0 0 black;
+  };
 `
 
 const BgDiv = styled.div`
-  background-color: #222;
+  border: 2px solid #264143;
+  box-shadow: 3px 4px 0px 1px #000000;
+  background-color: #D1D1D1;
   position: absolute;
   top: -1000px;
   left: -1000px;
@@ -113,4 +125,6 @@ const BgDiv = styled.div`
     width: 100%;
     height: 100%;
   }
+
+
 `
