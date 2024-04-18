5b3e7a85a417a13a8f18bad57ce6f7cea675d3e6
ElBuenParker <mr.gonzalex.4@gmail.com>
2024-04-18 01:04:13 -0600

    projects added

--- src/pages/Portafolio.jsx
+++ src/pages/Portafolio.jsx
@@ -5,31 +5,35 @@ import '../styles/bubble.css'
 import '../styles/portafolio.css'
 import FormsImg from '../img/forms.jpg'
 import ButtonsImg from '../img/btn.jpg'
+import { Link } from 'react-router-dom';
 
 const MainContainer = styled.div`
   display:flex;
   align-items: center;
   padding: 20px;
- 
 `;
+
+
 
 const Contenido = styled.div`
   display:flex;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: space-around;
+  align-items:center;
+  align-content: space-between;
+  justify-content: space-between;
   padding: 20px;
   display: flex;
-  height: 800px;
-  background-color:#ffff; //borrar al final
+  height: 1200px;
   margin-top:10%;
+  @media(min-width: 100px){
+      position: relative;
+      margin-top:1%;
+  
+      display: block;
+    }
+
 `;
 
-
-
 const Portafolio = () => {
-
-
   return (
     <MainContainer>
     <div className="screen">
@@ -43,13 +47,47 @@ const Portafolio = () => {
       <Contenido>
 
         <div className="ContenidoImg">  
-          <div className="descripcionImg">Formularios en diferentes presentaciones para uso publico, personalizables y de uso sencillo.</div>
-          <img height={400} className="Img" src={FormsImg}></img>
+          <div className="descripcionImg">Formularios</div>
+          <div class="card">
+            <div class="card-inner">
+              <div class="card-front">
+                <p>
+                <img height={400} className="Img" src={FormsImg}></img>
+                </p>
+              </div>
+              <div class="card-back">
+                <p>Formularios interactivos diseñados con HTML y estilizados con CSS. Estos formularios son altamente personalizables, permitiendo ajustes en diseño, campos requeridos y acciones de envío. Su estructura modular facilita la integración en diferentes proyectos web.  </p>
+                <Link to={"https://github.com/ElBuenParker/logins"}>
+                <button>
+                <span class="button_top" > Ir al código <ion-icon classname="gh-icon" name="logo-github"></ion-icon></span>
+              </button>
+                </Link>
+              </div>
+              
+            </div>
+          </div>
         </div>
 
         <div className="ContenidoBtn">  
-          <div className="descripcionImg">Botones con diferenetes estilos, personalisables y adaptables de manera sencilla.</div>
-          <img height={400} className="Img" src={ButtonsImg}></img>
+          <div className="descripcionImg">Botones Personalizados</div>
+          <div class="card">
+            <div class="card-inner">
+              <div class="card-front">
+                <p>
+                <img height={400} className="Img" src={ButtonsImg}></img>
+                </p>
+              </div>
+              <div class="card-back">
+                <p>Botones interactivos con estilos personalizables creados con CSS y JavaScript, diseñados para ser fácilmente editables y adaptables a diferentes estilos y necesidades. Su código permite ajustar colores, tamaño, efectos de hover y comportamiento con facilidad, proporcionando una solución versátil para agregar interactividad a páginas web.</p>
+                <Link to={"https://github.com/ElBuenParker/buttons"}>
+                <button>
+                <span class="button_top" > Ir al código <ion-icon classname="gh-icon" name="logo-github"></ion-icon></span>
+              </button>
+                </Link>
+               
+              </div>
+            </div>
+          </div>
         </div>
 
        
